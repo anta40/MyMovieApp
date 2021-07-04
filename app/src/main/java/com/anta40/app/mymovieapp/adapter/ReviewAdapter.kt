9839e6f7f1fb1d259120d6b0a51e0eefac7062dc
Andre Tampubolon <andre@lc.vlsm.org>
2021-07-05 06:40:00 +0700

    Penambahan layout dan adapter untuk Review

---
+++ app/src/main/java/com/anta40/app/mymovieapp/adapter/ReviewAdapter.kt
@@ -0,0 +1,43 @@
+package com.anta40.app.mymovieapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.anta40.app.mymovieapp.R
+import com.anta40.app.mymovieapp.model.Review
+
+class ReviewAdapter(private val context: Context, private val dataSet: List<Review>):
+    RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtReviewAuthor: TextView
+        val txtReviewContent: TextView
+        val txtReviewCreated: TextView
+
+        init {
+            txtReviewAuthor =  view.findViewById(R.id.txt_review_author)
+            txtReviewContent = view.findViewById(R.id.txt_review_content)
+            txtReviewCreated = view.findViewById(R.id.txt_review_created)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_review, parent, false)
+
+        return ReviewAdapter.ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.txtReviewAuthor.text = dataSet[position].author
+        holder.txtReviewContent.text = dataSet[position].content
+        holder.txtReviewCreated.text = dataSet[position].created_at
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+}
